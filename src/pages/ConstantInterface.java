8cd94afeca11b6d911aab0a949e5f94e0e8cba10
Sumit Soni <sumitsoni691@gmail.com>
2020-11-29 20:37:14 +0530

    pom file changes

--- src/pages/ConstantInterface.java
+++ src/pages/ConstantInterface.java
@@ -4,7 +4,7 @@ public class ConstantInterface {
 	
 	 public static final String strURL = "http://node-1.nginx.portal.da-1.rbcus.qa.aws.veriday.net/login";
 
-     public static final String Path_TestData = "/Veriday/source";
+     public static final String Path_TestData = "//source";
 
      public static final String File_TestData = "TestData.xlsx";
 
