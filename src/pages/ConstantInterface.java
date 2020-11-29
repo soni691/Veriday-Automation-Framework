f45b68c8172b7e12cb689e581dd2d85209120e87
sumit.soni <sumitsoni691@gmail.com>
2020-11-29 22:14:22 +0530

    added test data file path

--- src/pages/ConstantInterface.java
+++ src/pages/ConstantInterface.java
@@ -1,14 +1,21 @@
 package pages;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
 public class ConstantInterface {
 	public static Properties prop;
-	
+//	 prop = new Properties();
+//		FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/com/crm"
+//				+ "/qa/config/config.properties");
+//		prop.load(ip);
 	 public static final String strURL = "http://node-1.nginx.portal.da-1.rbcus.qa.aws.veriday.net/login";
-
-     public static final String Path_TestData = "";
+	 //public static String Path_TestData = "./src\\Veriday\\source";
+	 //public static String Path_TestData  = System.getProperty("user.dir"+"\\source");
+	
+	 //File src = new File(Path_TestData);
+     public static final String Path_TestData = "D:\\VeridayAutomationGit\\Veriday-Automation-Framework\\source";
 
      public static final String File_TestData = "TestData.xlsx";
      
